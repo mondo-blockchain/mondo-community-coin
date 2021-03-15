73ddcf7c14ee7f11fed2c3000ae39006d2dd15e9
Ramy Hardan <gitlab@hardan.de>
2021-03-15 14:39:43 +0100

    fix: Deployment script

--- deploy/00_erc20-vested.ts
+++ deploy/00_erc20-vested.ts
@@ -2,11 +2,7 @@ import { BigNumber } from "@ethersproject/bignumber";
 import { DeployFunction } from "hardhat-deploy/types";
 const BASE = BigNumber.from(10).pow(18);
 
-const func: DeployFunction = async ({
-  getNamedAccounts,
-  getUnnamedAccounts,
-  deployments,
-}) => {
+const func: DeployFunction = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer, r1, r2 } = await getNamedAccounts();
   console.log("***", deployer, r1, r2);
@@ -17,7 +13,6 @@ const func: DeployFunction = async ({
     args: [
       "Mondo Community Coin",
       "MNDCC",
-      deployer,
       BASE.mul("180000000"),
       [
         2592000,
